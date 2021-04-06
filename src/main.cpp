6fe39f822efd03fd975b2a2eb0e21e7bc332d3fe
glenchiridza <glenchiridz@gmail.com>
2021-04-06 21:36:00 +0200

    window screen

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,38 @@ using namespace sf;
 
 int main() {
 
+    //create video mode object
     VideoMode vm(1920,1080);
-    
+
+    //Create and open a window for the game
+    RenderWindow window(vm,"Timber Game",Style::Fullscreen);
+
+    while(window.isOpen()){
+        /*
+        handle player input
+        */
+       if(Keyboard::isKeyPressed(Keyboard::Escape)){
+           window.close();
+       }
+
+       /*
+       update scene
+       */
+
+       /*
+       draw scene
+       */
+
+      //clear window from last frame
+      window.clear();
+
+      //draw game scene
+
+      
+      //show drawing
+      window.display();
+
+    }
+
+
   }
