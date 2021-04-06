7cc43fe78a3060f14cca887b69e4b034dbd42591
glenchiridza <glenchiridz@gmail.com>
2021-04-06 22:23:56 +0200

    add sprites

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,64 @@ int main() {
     //Create and open a window for the game
     RenderWindow window(vm,"Timber Game",Style::Fullscreen);
 
+    //create texture to hold graphic on GPU
+    Texture textureBackground;
+
+    //load graphic into the texture
+    textureBackground.loadFromFile("graphics/background.png");
+
+    //creat sprite
+    Sprite spriteBackground;
+
+    //attach texture to sprite
+    spriteBackground.setTexture(textureBackground);
+
+    //set sprite background to cover screen
+    spriteBackground.setPosition(0,0);
+
+    //,make a tree sprite
+    Texture textureTree;
+    textureTree.loadFromFile("graphics/tree.png");
+    Sprite spriteTree;
+    spriteTree.setTexture(textureTree);
+    spriteTree.setPosition(810,0);
+
+    //prepare bee
+    Texture textureBee;
+    textureBee.loadFromFile("graphics/bee.png");
+    Sprite spriteBee;
+    spriteBee.setTexture(textureBee);
+    spriteBee.setPosition(0,800);
+
+    //is bee currently moving
+    bool beeActive = false;
+
+    //how fast can the bee fly
+    float beeSpeed = 0.0f;
+
+    //make 3 cloud sprites from one texture
+    Texture textureCloud;
+    textureCloud.loadFromFile("graphics/cloud.png");
+
+    //3 sprites
+    Sprite spriteCloud1;
+    Sprite spriteCloud2;
+    Sprite spriteCloud3;
+
+    spriteCloud1.setTexture(textureCloud);
+    spriteCloud2.setTexture(textureCloud);
+    spriteCloud3.setTexture(textureCloud);
+
+    //position clouds to the left of the screen
+    //at different heights
+    spriteCloud1.setPosition(0,0);
+    spriteCloud2.setPosition(0,250);
+    spriteCloud3.setPosition(0,500);
+
+    //are clouds currently on screen
+
+
+
     while(window.isOpen()){
         /*
         handle player input
@@ -31,7 +89,7 @@ int main() {
       window.clear();
 
       //draw game scene
-
+      window.draw(spriteBackground);
       
       //show drawing
       window.display();
